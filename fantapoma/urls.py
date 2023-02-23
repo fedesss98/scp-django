5e7d0250d90688ca3fea6c974d2ad9a5023862ad
Federico Amato <federico.amato1998@gmail.com>
2023-02-23 07:45:41 +0100

    Add rules and other

--- fantapoma/urls.py
+++ fantapoma/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
         path('leaderboard', LeaderboardView.as_view(), name='leaderboard'),
         path('view_crew/<str:pk>/', ViewCrew.as_view(), name='view-crew'),
         path('view_crew/', ViewCrew.as_view(), name='view-crew'),
-
         path('create_special', CreateSpecialView.as_view(), name='create-special'),
         path('view_specials', ListSpecialsView.as_view(), name='view-specials')
     ]
