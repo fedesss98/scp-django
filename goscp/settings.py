34216ef93f71b84af6d19b7669f554bc70cbfce0
Federico Amato <federico.amato1998@gmail.com>
2024-02-10 11:10:09 +0100

    Add new csrf trusted origin

--- goscp/settings.py
+++ goscp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['web-production-100c.up.railway.app', 'web-production-acb4f.up.railway.app', 'goscp.it', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-100c.up.railway.app', 'https://goscp.it']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-100c.up.railway.app', 'https://web-production-acb4f.up.railway.app', 'https://goscp.it']
 
 # Application definition
 
