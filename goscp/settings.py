af8dde0281eb32ec11461b2c54e4240b976ad5f4
Federico Amato <federico.amato1998@gmail.com>
2024-04-13 21:46:35 +0200

    Add fantapoma.it origin

--- goscp/settings.py
+++ goscp/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['web-production-100c.up.railway.app', 'web-production-acb4f.up.railway.app', 'goscp.it', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-100c.up.railway.app', 'https://web-production-acb4f.up.railway.app', 'https://goscp.it']
+ALLOWED_HOSTS = ['web-production-100c.up.railway.app', 'web-production-acb4f.up.railway.app', 'goscp.it', 'www.fantapoma.it','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-100c.up.railway.app', 'https://web-production-acb4f.up.railway.app', 'https://goscp.it', 'http://www.fantapoma.it',]
 
 # Application definition
 
